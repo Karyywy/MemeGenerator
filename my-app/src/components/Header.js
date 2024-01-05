34951a219ae83cb0281049ef1763382989111c2c
Kary Young <128840281+Karyywy@users.noreply.github.com>
2024-01-05 14:41:52 +0800

    create navigation bar

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Header(){
+    return (
+        <header className="header">
+            <img
+                src="./images/header-logo.png"
+                className="header--logo"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <ul className="header--container">
+            <li className="header--tab"><a href="#">Download</a></li>
+            <li className="header--tab"><a href="#">Share</a></li>
+            <li className="header--tab"><a href="/">Refresh</a></li>
+            </ul>
+        </header>
+    )
+}
